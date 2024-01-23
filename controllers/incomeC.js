accf6afb3c5d9b961e35184d2a84a13be275d55b
Soumya1002 <soumyasenthil1002@gmail.com>
2024-01-23 15:27:56 +0530

    tas10: premium features-leaderboard

---
+++ controllers/incomeC.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+const sequelize = require('./util/database');
+const User = require('./models/user');
+const Expense = require('./models/expenses');
+const Income = require('./models/incomes');
+const Order = require('./models/orders');
+
+//Routers File Imports
+const signupRoutes = require('./routes/signupR');
+const loginRoutes = require('./routes/loginR');
+const expenseRoutes = require('./routes/expenseR');
+const incomeRoutes = require('./routes/incomeR');
+const purchaseRoutes = require('./routes/purchaseR');
+const premiumRoutes = require('./routes/premiumR');
+
+app.use(cors());
+app.use(bodyParser.json({ extended: false }));
+
+app.use(signupRoutes);
+app.use(loginRoutes);
+app.use('/expense',expenseRoutes);
+app.use('/income',incomeRoutes);
+app.use('/purchase',purchaseRoutes);
+app.use('/premium', premiumRoutes);
+
+//tables relationship
+User.hasMany(Expense);  // one to many
+Expense.belongsTo(User) //one to one 
+
+User.hasMany(Income);  // one to many
+Income.belongsTo(User) //one to one 
+
+User.hasMany(Order);
+Order.belongsTo(User);
+
+//When force: true is set, it will drop the existing tables from the database and recreate them, effectively resetting the database schema.
+sequelize.sync()
+    .then(result => {
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err);
+    })
